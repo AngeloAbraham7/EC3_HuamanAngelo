4bb5f07bec1a891e797f5519631dce498010127d
AngeloAbraham7 <angeloangelohuamanlapiz@hotmail.com>
2023-11-20 17:13:18 -0500

    Primer commit

---
+++ app/Review.java
@@ -0,0 +1,17 @@
+public class Review {
+    private String title;
+    private String description;
+
+    public Review(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
---
+++ app/ReviewAdapter.java
@@ -0,0 +1,47 @@
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ReviewAdapter extends RecyclerView.Adapter<ReviewAdapter.ReviewViewHolder> {
+
+    private List<Review> reviewList;
+
+    public ReviewAdapter(List<Review> reviewList) {
+        this.reviewList = reviewList;
+    }
+
+    @NonNull
+    @Override
+    public ReviewViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_review, parent, false);
+        return new ReviewViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ReviewViewHolder holder, int position) {
+        Review review = reviewList.get(position);
+        holder.textTitle.setText(review.getTitle());
+        holder.textDescription.setText(review.getDescription());
+    }
+
+    @Override
+    public int getItemCount() {
+        return reviewList.size();
+    }
+
+    public static class ReviewViewHolder extends RecyclerView.ViewHolder {
+        TextView textTitle, textDescription;
+
+        public ReviewViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textTitle = itemView.findViewById(R.id.textTitle);
+            textDescription = itemView.findViewById(R.id.textDescription);
+        }
+    }
+}
